9ea08efb81522f6ab3517b4fc8ebc8492e869e7c
BS263 <sifathasnain@yahoo.com>
2020-03-24 12:42:46 +0600

    fixed file download

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # DCM_API
 #### DCM file serving api 
 #### Test api using 
+* python app.py
 * curl http://127.0.0.1:5000/file-serve/ID_3fa17b45b.dcm
--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify, abort, send_from_directory
+import os
+import urllib.request
+from flask import Flask, request, redirect, jsonify, send_from_directory, abort
+from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'uploads'
 
@@ -6,9 +9,32 @@ app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+ALLOWED_EXTENSIONS = set(['dcm'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/file-serve/<path:filename>')
+def upload_file(filename):
+	# check if filename is empty
+	if filename == '':
+		resp = jsonify({'message' : 'File '})
+		resp.status_code = 400
+		return resp
+	if allowed_file(filename):
+		try:
+			return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+			 
+		except FileNotFoundError:
+			abort(404)
+	else:
+		resp = jsonify({'message' : 'Allowed file type is dcm'})
+		resp.status_code = 400
+		return resp
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
-                               
+
+                
 
